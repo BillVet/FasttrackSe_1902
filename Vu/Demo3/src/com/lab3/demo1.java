ac2bab73199da7bb9f4413711379ba01be9645d3
NguyenHuyVu <hvucool@gmail.com>
2019-11-15 11:17:28 +0700

    Lab3

--- Vu/Demo3/src/com/lab3/demo1.java
+++ Vu/Demo3/src/com/lab3/demo1.java
@@ -4,5 +4,22 @@ public class demo1 {
 
 	public static void main(String[] args) {
 		
+		int number = 7;
+		for(int i = 1 ; i<=10;i++) {
+			System.out.println(number+ " x " + i + "=" +number*i);
+		}
+		
+		int j = 1;
+		while(j<=10) {
+			System.out.printf("%d x %d = %d %n",number,j,number*j);
+			j++;
+		}
+		
+		int k =1;
+		do {
+			System.out.printf("%d x %d = %d %n",number,k,number*k);
+			k++;
+			
+		} while (k<=10);
 	}
 }
