91bd4507bdf8caaf7b744169358beb9e8ebec770
NguyenHuyVu <hvucool@gmail.com>
2019-11-18 08:47:38 +0700

    Lab3

---
+++ Vu/Demo3/src/com/lab3/baitap/B2.java
@@ -0,0 +1,14 @@
+package com.lab3.baitap;
+
+public class B2 {
+
+	public static void main(String[] args) {
+		int i,j;
+		for(i=1;i<=9;i++) {
+			for(j=1;j<=10;j++) {
+				System.out.printf("%d x %d = %d %n",i,j,i*j);
+			}
+			System.out.println("=============");
+		}
+	}
+}
