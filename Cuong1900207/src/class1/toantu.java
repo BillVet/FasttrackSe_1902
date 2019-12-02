cb36310e63af56dfc5ffc658a3bca9e85b9dd922
cuong <1900207@fasttrack.edu.vn>
2019-12-02 17:00:50 +0700

    2/12/2019

---
+++ Cuong1900207/src/class1/toantu.java
@@ -0,0 +1,24 @@
+package class1;
+
+import java.util.Scanner;
+
+public class toantu {
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int tong;
+				
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Nhap so a");
+		a = scanner.nextInt();
+		
+		System.out.println("Nhap so b");
+		b = scanner.nextInt();
+		
+		tong=a + b;
+		System.out.println("Tong 2 so la: " + tong);
+		System.out.println("Binh phuong tong la: " + Math.pow(tong,2));
+		System.out.println("can bac 2 tong la: " + Math.sqrt(tong));
+		}
+}
