c0e3021fc74c5dfd89654e3f7d864a5d8115649a
honghanh <buithihonghanh345@gmail.com>
2019-12-10 00:32:53 +0700

    bài tập

---
+++ Hanh/Hello/src/baitap/bai5.java
@@ -0,0 +1,30 @@
+package baitap;
+
+import java.util.Scanner;
+
+public class bai5 {
+	public static void main(String[]args) {
+		
+		Scanner scanner= new Scanner(System.in);
+		int x =8;
+		for(int i=1; i<=10;i++) {
+			System.out.println("x x "+i+"="+x*i);
+			System.out.printf("%d x %d = %d %n",x,i,x*i);
+		}
+	}
+public static void main1(String[]args) {
+		
+		Scanner scanner= new Scanner(System.in);
+		int I =9;
+		for(int i=1; i<=9;i++) {
+			System.out.printf("%d x %d = %d %n",I,i,I*i);
+		}
+		}
+public static void main2(String[]args) {
+	
+	Scanner scanner= new Scanner(System.in);
+	int  j=9;
+	for(int i=1; i<=10;i++) {
+		System.out.printf("%d x %d = %d %n",j,i,j*i);
+	}
+}}
