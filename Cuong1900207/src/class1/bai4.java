3401da0c90b2c3cbad5801b511fc2effb20370d1
cuong <1900207@fasttrack.edu.vn>
2019-12-09 13:34:24 +0700

    3

--- Cuong1900207/src/class1/bai4.java
+++ Cuong1900207/src/class1/bai4.java
@@ -1,5 +1,24 @@
 package class1;
-
+import java.util.Scanner;
 public class bai4 {
-
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		double delta;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Nhap a");
+		a = scanner.nextInt();
+		System.out.println("Nhap b");
+		b = scanner.nextInt();
+		System.out.println("Nhap c");
+		c = scanner.nextInt();
+		 
+		delta = Math.pow(b,2)-(4*a*c);
+		
+		System.out.println("Delta la: " + delta);
+		System.out.println("Can Delta la: " + Math.sqrt(delta));
+	}
 }
