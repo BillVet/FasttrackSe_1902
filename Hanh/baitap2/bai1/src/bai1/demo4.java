322f2c04b0f1d74468fe106014d550ef70ff4c1f
honghanh <buithihonghanh345@gmail.com>
2019-12-11 13:07:24 +0700

    bai

---
+++ Hanh/baitap2/bai1/src/bai1/demo4.java
@@ -0,0 +1,29 @@
+package bai1;
+
+import java.util.Scanner;
+
+public class demo4 {
+	public	String tensp ;
+	public double dongia;
+	public double giamgia;
+	Scanner scanner=new Scanner(System.in);
+	public void output() { 
+		System.out.println(tensp);
+		System.out.println(dongia);
+		System.out.println(giamgia);
+	}
+	public void input() {
+		System.out.println("nhap ten sp");
+		tensp= scanner.nextLine();
+		System.out.println("nhap don gia");
+		dongia= scanner.nextDouble();
+		System.out.println("sp giam gia");
+		giamgia = scanner.nextDouble();
+		
+	}
+	public void getThueNhapKhau() {
+		double b;
+		b = ( dongia * 10)/100;
+		System.out.println(b);
+	}
+}
